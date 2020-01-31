6c871c9c8ad454e0f2d0acb3c1d8b241fa4625b9
FAyupov <kppfsb@mail.com>
2020-01-31 17:59:55 +0300

    initial commit made rating of programming languages

--- rating.rb
+++ rating.rb
@@ -1,7 +1,16 @@
 require 'json'
 
 path = File.read('rating.json')
-
 languages_list = JSON.parse(path)
 
-puts languages_list
+sort_list = languages_list.sort_by { |key, value| - value }
+
+popular_languages = sort_list[0]
+puts "Самый популярный язык: #{popular_languages[0]} (#{popular_languages[1]})"
+
+number_string = 0
+sort_list.each do |languages, score|
+  number_string +=1
+  puts "#{number_string}: #{languages} (#{score})"
+end
+
